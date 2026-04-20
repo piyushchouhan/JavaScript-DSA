49cadd146437edde56d2fb3711a9731cab84696c
Piyush Chauhan <piyush9695@gmail.com>
2026-04-20 10:36:40 +0530

    valid-parantheses.js added

---
+++ valid-parantheses.js
@@ -0,0 +1,28 @@
+function isValid(s){
+    let stack = [];
+    const map = {
+        ')':'(',
+        '}':'{',
+        ']':'['
+    }
+
+    for(let char of s ){
+        if(char === '(' || char === '{' || char === '['){
+            stack.push(char);
+        }else{
+            if(stack.length !== 0 && stack.pop() !== map[char]){
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+}
+
+
+
+console.log(isValid("()"));        // true
+console.log(isValid("()[]{}"));    // true
+console.log(isValid("(]"));        // false
+console.log(isValid("([)]"));      // false
+console.log(isValid("{[]}"));      // true
