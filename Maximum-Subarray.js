78579db930e86c6d592729dcc735ab7e593ed45c
Piyush Chauhan <piyush9695@gmail.com>
2026-04-22 13:19:28 +0530

    Maximum-Subarray.js added

---
+++ Maximum-Subarray.js
@@ -0,0 +1,16 @@
+// Find the contigous subarray (containing at least one number) which has the larget sum, and return its sum
+// Kadane's algorithm
+
+function maxSubArray(nums){
+    let maxSum = nums[0];
+    let currentSum = nums[0];
+    
+    for(let i = 0; i < nums.length; i++){
+        currentSum = Math.max(nums[i], currentSum+nums[i]);
+        maxSum = Math.max(maxSum, currentSum);
+    }
+
+    return maxSum;
+}
+
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
