04accbc16548089ab41cb4b5b31d1464c1a336e5
Piyush Chauhan <piyush9695@gmail.com>
2026-04-23 21:42:26 +0530

    Binary Search is added

--- test.js
+++ test.js
@@ -1,10 +1,5 @@
-function foo() {
-    console.log(this.a);
+var myObject = {
+    value: 2,
 }
-var obj = {
-    a: 2,
-    foo: foo
-};
-var bar = obj.foo; // function reference/alias!
-var a = "oops, global"; // `a` also property on global object
-bar(); // "oops, global"
+
+Object.getOwnPropertyDescriptor(myObject, 'value');
