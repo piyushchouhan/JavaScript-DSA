87374cfe4a7cca410359068a29e77c2778515300
Piyush Chauhan <piyush9695@gmail.com>
2026-05-05 09:03:05 +0530

    selection-sort.js added

---
+++ selection-sort.js
@@ -0,0 +1,21 @@
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex !== i) {
+            let temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
+
+// use the function
+let arr = [64, 25, 12, 22, 11];
+selectionSort(arr);
+console.log("Sorted array: " + arr);
