bb389d2a0c0c678c757821efc7536e30c9cf604f
Piyush Chauhan <piyush9695@gmail.com>
2026-06-08 21:05:11 +0530

    feat: implement interactive CLI for binary search input and target retrieval

--- Binary-Search.js
+++ Binary-Search.js
@@ -1,13 +1,44 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
 function binarySearch(arr, target) {
     let left = 0;
     let right = arr.length - 1;
     while (left <= right) {
         let mid = Math.floor((left + right) / 2);
-        if(arr[mid] === target) return mid;
-        if(arr[mid] < target) left = mid + 1;
+        if (arr[mid] === target) return mid;
+        if (arr[mid] < target) left = mid + 1;
         else right = mid - 1;
     }
     return -1;
 }
 
-console.log(binarySearch([1, 2, 3, 4, 5], 3));
+rl.question('Enter sorted number seperated by space ',
+    (arrayInput) => {
+        const arr = arrayInput.trim().split(" ").map(Number);
+
+        // ask the target
+        rl.question('enter the target: ',
+            (targetInput) => {
+                const target = Number(targetInput);
+
+                // perform the search
+                const result = binarySearch(arr, target);
+
+                if (result !== -1) {
+                    console.log(`Target ${target} found at index ${result}.`);
+                } else {
+                    console.log(`Target ${target} not found.`);
+                }
+
+                rl.close();
+
+
+            }
+        )
+    }
+)
