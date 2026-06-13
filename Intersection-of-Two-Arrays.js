a963087303a09a7d6b7aaa246b7a567c131b9d4a
Piyush Chauhan <piyush9695@gmail.com>
2026-06-13 22:32:38 +0530

    feat: add readline interface to accept dynamic user input for array intersection

--- Intersection-of-Two-Arrays.js
+++ Intersection-of-Two-Arrays.js
@@ -1,3 +1,10 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 function intersection(arr1, arr2){
     let set1 = new Set(arr1);
     let result = new Set();
@@ -11,5 +18,16 @@ function intersection(arr1, arr2){
     return Array.from(result);
 }
 
+rl.question('Enter the first array of numbers (separated by spaces): ', (input1) => {
+    rl.question('Enter the second array of numbers (separated by spaces): ', (input2) => {
+        let arr1 = input1.trim().split(/\s+/).map(Number);
+        let arr2 = input2.trim().split(/\s+/).map(Number);
 
-console.log(intersection([1,2,2,1], [2,2]));
+        if (arr1.some(isNaN) || arr2.some(isNaN)) {
+            console.log("Please enter valid numbers.");
+        } else {
+            console.log("Intersection:", intersection(arr1, arr2));
+        }
+        rl.close();
+    });
+});
